45462aa44660c4fdcad31d2b992c59d9c3cd0b4b
azskrzysiek <azskrzysiek@gmail.com>
2019-05-08 15:00:34 +0200

    Custom event answer delete

--- public/js/app.js
+++ public/js/app.js
@@ -3782,11 +3782,7 @@ __webpack_require__.r(__webpack_exports__);
         position: 'center',
         buttons: [['<button><b>YES</b></button>', function (instance, toast) {
           axios["delete"](_this2.endpoint).then(function (res) {
-            $(_this2.$el).fadeOut(500, function () {
-              _this2.$toast.success(res.data.message, "Success", {
-                timeout: 3000
-              });
-            });
+            _this2.$emit('deleted');
           });
           instance.hide({
             transitionOut: 'fadeOut'
@@ -3865,6 +3861,10 @@ function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
     this.fetch("/questions/".concat(this.questionId, "/answers"));
   },
   methods: {
+    remove: function remove(index) {
+      this.answers.splice(index, 1);
+      this.count--;
+    },
     fetch: function fetch(endpoint) {
       var _this = this;
 
@@ -41768,10 +41768,15 @@ var render = function() {
                   _vm._v(" "),
                   _c("hr"),
                   _vm._v(" "),
-                  _vm._l(_vm.answers, function(answer) {
+                  _vm._l(_vm.answers, function(answer, index) {
                     return _c("answer", {
                       key: answer.id,
-                      attrs: { answer: answer }
+                      attrs: { answer: answer },
+                      on: {
+                        deleted: function($event) {
+                          return _vm.remove(index)
+                        }
+                      }
                     })
                   }),
                   _vm._v(" "),
